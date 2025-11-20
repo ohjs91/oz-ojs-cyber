0724ab23cf90fb7562451930547a0f453d7bbb86
진석 <ozjinseok@gmail.com>
2025-11-20 21:25:06 +0900

    #9 Btn 작업

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -13,7 +13,6 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -28,4 +27,5 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  prettierPlugin,
 ]);
